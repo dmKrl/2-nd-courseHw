2d4ec589a81728ebe252796af0f4ecc9963549ee
Dmitry Kralichkin <dm_krl@mail.ru>
2023-05-11 23:54:53 +1000

    ready js hw and started mobile version.

---
+++ hw-1.js
@@ -0,0 +1,92 @@
+// Task-1
+
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+
+
+// Task-2
+
+// const yearRelease = 2007;
+// alert(yearRelease);
+
+
+// Task-3
+
+// const firstName = 'Brendan';
+// const lastName = 'Ike';
+// alert(`${firstName} ${lastName}`);
+
+
+// Task-4
+
+// const a = 10;
+// const b = 2;
+
+// alert(`sum = ${a + b}`);
+// alert(`mult = ${a * b}`);
+// alert(`diff = ${a - b}`);
+// alert(`div = ${a / b}`);
+
+
+// Task-5
+
+// const result = Math.pow(2, 5);
+// alert(result);
+
+
+// Task-6
+
+// const a = 9;
+// const b = 2;
+
+// alert(a % b);
+
+
+// Task-7
+
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+
+
+// Task-8
+
+// const age = prompt('Сколько вам лет?')
+// alert(age);
+
+
+// Task-9
+
+// const user = {
+//     name: 'Dmitry',
+//     age: 22,
+//     isAdmin: false
+// }
+
+// // Task-9.1
+// user.cityOfResidence = true;
+
+// // Task-9.2
+// user.age = 23;
+
+// // Task-9.3
+// delete user.cityOfResidence;
+// console.log(user.cityOfResidence)
+
+// // Task-9.4
+// let info = prompt('Какую информацию хотите узнать о пользователе?')
+// alert(user[info]);
+
+
+// Task-10
+
+// let ask = prompt('Как вас зовут?');
+
+// alert(`Привет, ${ask}!`);
