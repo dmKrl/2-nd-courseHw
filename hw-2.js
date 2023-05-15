2ee49bb68171ca4e459356045920e108fe6a843d
Dmitry Kralichkin <dm_krl@mail.ru>
2023-05-15 21:15:43 +1000

    ready hw-2(js) and    mobile adaptive

---
+++ hw-2.js
@@ -0,0 +1,133 @@
+// Task-1
+
+// let password = 'пароль';
+
+// let askPassword = prompt('Введите пароль');
+
+// alert(askPassword === password 
+    // ? 'Пароль введен верно' 
+    // : 'Пароль введён неверно');
+
+
+
+// Task-2
+
+// const c = Number(prompt('Введите число'));
+
+// console.log(c >= 0 && c <= 10 
+    // ? 'Верно' 
+    // : 'Неверно');
+
+
+
+// Task-3
+
+// const d = 2;
+// const e = 101;
+
+// console.log(d > 100 || e > 100 
+    // ? 'Верно' 
+    // : 'Неверно')
+
+
+
+// Task-4
+
+// let a = '2';
+// let b = '3';
+
+// alert(Number(a) + Number(b));
+
+
+
+// Task-5
+
+// const monthNumber = 10;
+
+// switch (monthNumber) {
+//     case 12:
+//         console.log('Зима')
+//         break;
+//     case 1:
+//         console.log('Зима');
+//         break;
+//     case 2:
+//         console.log('Зима');
+//         break;
+//     case 3:
+//         console.log('Весна')
+//         break;
+//     case 4:
+//         console.log('Весна');
+//         break;
+//     case 5:
+//         console.log('Весна');
+//         break;    
+//     case 6:
+//         console.log('Лето')
+//         break;
+//     case 7:
+//         console.log('Лето');
+//         break;
+//     case 8:
+//         console.log('Лето');
+//         break;
+//     case 9:
+//         console.log('Осень')
+//         break;
+//     case 10:
+//         console.log('Осень');
+//         break;
+//     case 11:
+//         console.log('Осень');
+//         break;
+//     default:
+//         console.log('Такого месяца нет!')
+//         break;
+// }
+
+
+
+// Task-7
+
+// let num = 12123;
+
+// console.log((typeof num) === 'number' 
+//     ? num 
+//     : 'NaN');
+
+// if (num % 2 === 0) {
+//     alert('Число чётное');
+// } else {
+//     alert('Число нечётное');
+// }
+
+
+
+// Task-8
+
+// const clientOS = 1;
+
+// console.log(clientOS === 1 
+//     ? 'Установите версию приложения для IOS по ссылке' 
+//     : 'Установите версию приложения для Android по ссылке');
+
+
+
+// Task-9 
+
+// const clientDeviceYear = prompt('Введите год создания вашего устройства');
+// const clientOS = prompt('Введите названте вашей операционной системы');
+
+// if (clientDeviceYear >= 2015 && clientOS === 1) {
+//     alert('Установите версию приложения для Android по ссылке');
+// }
+// if (clientDeviceYear >= 2015 && clientOS === 0) {
+//     alert('Установите версию приложения для IOS по ссылке');
+// }
+// if (clientDeviceYear < 2015 && clientOS === 1) {
+//     alert('Установите облегчённую версию приложения для Android по ссылке');
+// }
+// if (clientDeviceYear < 2015 && clientOS === 0) {
+//     alert('Установите облегчённую версию приложения для IOS по ссылке');
+// }
